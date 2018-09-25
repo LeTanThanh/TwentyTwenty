0a93900dc6ae9ff487758801d78546fa8ac15c66
LeTanThanh <tanthanhcr95@gmail.com>
2018-09-26 00:06:03 +0700

    TwentyTwenty

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('.twentytwenty-container').twentytwenty({
+    default_offset_pct: 0.5,
+    orientation: 'horizontal',
+    before_label: 'January 2017',
+    after_label: 'March 2017',
+    no_overlay: false,
+    move_slider_on_hover: false,
+    move_with_handle_only: true,
+    click_to_move: true
+  });
+});
